085f762c6795bff47508eecba7bf4e9f5de74446
Patrick Miner <patrickminer318@gmail.com>
2017-09-15 17:06:36 -0700

    Added Rebuttle functionality so Player 2 has the same number of chances to reach goal as Player 1

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
     
+
     // Update roundScore if # is not 1
     if (dice !== 1) {
       // Add dice value to roundScore
@@ -31,34 +32,61 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
       // Make player's current score visible and equal to round score
       document.getElementById('current-' + activePlayer).textContent = roundScore;
     } else {
-      nextPlayer();
+      if (rebuttleRound) {
+        // If it's rebuttle round and Player 2 Rolls a 1 it's game over
+        endGame();
+      } else {
+        nextPlayer();
+      }
     }
   }
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
   if (gamePlaying) {  
-    // Add current score to global score
-    scores[activePlayer] += roundScore;
+    if (rebuttleRound === false) {
+      // Add current score to global score
+      scores[activePlayer] += roundScore;
     
-    // Update UI of global score
-    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+      // Update UI of global score
+      document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
 
-    // Check to see if theres a winner => if no winner nextPlayer()
-    if (scores[activePlayer] >= goalScore) {
-      if (activePlayer === 1) {
-        endGame();
-        // gamePlaying = false;
-        // document.getElementById('name-' + activePlayer).textContent = 'Winner!';
-        // document.querySelector('.dice').style.display = 'none';
-        // document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-        // document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      // Check to see if theres a winner => if no winner nextPlayer()
+      if (scores[activePlayer] >= goalScore) {
+        // Check to see if its Player 2 that won => If true then no rebuttle round
+        if (activePlayer === 1) {
+          endGame();
+          // gamePlaying = false;
+          // document.getElementById('name-' + activePlayer).textContent = 'Winner!';
+          // document.querySelector('.dice').style.display = 'none';
+          // document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+          // document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        } else {
+          // If it was Player 1 that one, Player 2 gets one last chance at Rebuttle
+          rebuttleRound = true;
+          document.getElementById('name-1').textContent = 'REBUTTLE!';
+          nextPlayer();
+        }
       } else {
         nextPlayer();
       }
     } else {
-      nextPlayer();
-      rebuttleRound = true;
+      // THIS IS WHAT HAPPENS IF IT'S THE REBUTTLE ROUND
+      if (activePlayer !== 1) { throw Error; }  // To ensure only player 2 is in in rebuttle mode
+      // Add current score to global score
+      scores[activePlayer] += roundScore;
+      // Update UI of global score
+      document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+      // See if Player 2's score is greater than goal
+      if (scores[activePlayer] >= goalScore) {
+      // If greater than goal, goalScore += 10, rebuttleround = false, next move
+        goalScore += 10;
+        rebuttleRound = false;
+        document.getElementById('name-1').textContent = 'Player 2';
+        nextPlayer();
+      } else {
+        endGame();
+      }
     }
   }
 })
@@ -74,8 +102,6 @@ function nextPlayer() {
   // Switch 'active class'
   document.getElementsByClassName('player-0-panel')[0].classList.toggle('active');
   document.getElementsByClassName('player-1-panel')[0].classList.toggle('active');
-  // Make dice disapear <-- COMMENTED OUT => I LIKE IT BETTER WHEN YOU CAN SEE THE DICE WITH 1 ROLLED
-  //diceDOM.style.display = 'none';
 }
 
 document.querySelector('.btn-new').addEventListener('click',init);
@@ -85,6 +111,7 @@ function init() {
   // Resets initial scores
   gamePlaying = true;
   rebuttleRound = false;
+  goalScore = 20;
   scores = [0,0];
   roundScore = 0;
   activePlayer = 0;
@@ -106,11 +133,19 @@ function init() {
 }
 
 function endGame() {
-  gamePlaying = false;
-  document.getElementById('name-' + activePlayer).textContent = 'Winner!';
-  document.querySelector('.dice').style.display = 'none';
-  document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-  document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+  if (rebuttleRound === false) {
+    gamePlaying = false;
+    document.getElementById('name-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+  } else {
+    gamePlaying = false;
+    document.getElementById('name-0').textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('winner');
+  }
 }
 
 
